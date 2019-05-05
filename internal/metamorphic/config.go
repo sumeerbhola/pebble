c77f7def2ad64eaef94f52261d5411bb377072e9
Peter Mattis <petermattis@gmail.com>
2019-05-05 17:02:04 -0400

    Metamorphic test
    Randomly generate a sequence of operations, and then replay this
    sequence repeatedly with random options, flushes, and compactions,
    verifying the test history is the same on each run.

---
+++ internal/metamorphic/config.go
@@ -0,0 +1,16 @@
+// Copyright 2019 The LevelDB-Go and Pebble Authors. All rights reserved. Use
+// of this source code is governed by a BSD-style license that can be found in
+// the LICENSE file.
+
+package metamorphic
+
+type config struct {
+	// TODO(peter): unimplemented
+	// ops            *randvar.Deck
+	// keyDist        randvar.Dynamic
+	// keySizeDist    randvar.Static
+	// valueSizeDist  randvar.Static
+	// updateFrac     float64
+	// lowerBoundFrac float64
+	// upperBoundFrac float64
+}
